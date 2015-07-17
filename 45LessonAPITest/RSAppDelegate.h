cad2bb579a118c15d0b05cef7ffaf5e70552c4ea
Romito2015 <romito@live.ru>
2015-07-17 15:44:44 +0300

    Initial Commit

---
+++ 45LessonAPITest/RSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RSAppDelegate.h
+//  45LessonAPITest
+//
+//  Created by Roman Chopovenko on 17.07.15.
+//  Copyright (c) 2015 Roman Chopovenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 45LessonAPITest/RSViewController.h
@@ -0,0 +1,13 @@
+//
+//  RSViewController.h
+//  45LessonAPITest
+//
+//  Created by Roman Chopovenko on 17.07.15.
+//  Copyright (c) 2015 Roman Chopovenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSViewController : UIViewController
+
+@end
